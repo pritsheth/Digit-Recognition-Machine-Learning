607e4c4b843c60ee4470361329b1ce2ef7a58126
psheth <pritsheth99@gmail.com>
2018-03-19 15:37:44 -0700

    Updating k means clustering

--- KmeansClustering/KMeans_Clustering.py
+++ KmeansClustering/KMeans_Clustering.py
@@ -45,21 +45,6 @@ class KNN:
         # print(np.array(t_data,object))
         return t_data, test_data
 
-    def display_digit(digit, labeled=True, title=""):
-        """
-        graphically displays a 784x1 vector, representing a digit
-        """
-        if labeled:
-            digit = digit[1]
-        image = digit
-        plt.figure()
-        fig = plt.imshow(image.reshape(28, 28))
-        fig.set_cmap('gray_r')
-        fig.axes.get_xaxis().set_visible(False)
-        fig.axes.get_yaxis().set_visible(False)
-        if title != "":
-            plt.title("Inferred label: " + str(title))
-
     # Computing mean of the cluster to form the new mean
     def init_new_centroids(self, train_data, K):
         centroids = random.sample(train_data, K)
